58881433ee903e44074f733ddfb0a27e524ad534
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-01-25 17:11:32 +0800

    Archive note and its  children recursively

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,6 +1,50 @@
 import { v } from "convex/values";
 
+import { Id } from "@/convex/_generated/dataModel";
 import { mutation, query } from "@/convex/_generated/server";
+
+export const archive = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+    if (!existingDocument) {
+      throw new Error("Not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const recursiveArchive = async (documentId: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) =>
+          q.eq("userId", userId).eq("parentDocument", documentId),
+        )
+        .collect();
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, { isArchived: true });
+        await recursiveArchive(child._id);
+      }
+    };
+
+    const document = await ctx.db.patch(args.id, {
+      isArchived: true,
+    });
+
+    recursiveArchive(args.id);
+
+    return document;
+  },
+});
 
 export const getTotalCount = query({
   handler: async (ctx) => {
@@ -13,9 +57,7 @@ export const getTotalCount = query({
 
     const documents = await ctx.db
       .query("documents")
-      .withIndex("by_user_parent", (q) => 
-        q.eq("userId", userId)
-      )
+      .withIndex("by_user_parent", (q) => q.eq("userId", userId))
       .filter((q) => q.eq(q.field("isArchived"), false))
       .collect();
 

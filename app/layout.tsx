02a1a476d93a7bf03b016667fd9709a03111ad4e
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-02-01 07:24:03 +0800

    Setup edgestore to handle file uploads

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { ConvexClientProvider } from "@/components/convex-provider";
 import { ModalProvider } from "@/components/modal-provider";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { EdgeStoreProvider } from "@/lib/edgestore";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -38,17 +39,19 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ConvexClientProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            storageKey="theme"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Toaster />
-            <ModalProvider />
-            {children}
-          </ThemeProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              storageKey="theme"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <Toaster />
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
         </ConvexClientProvider>
       </body>
     </html>

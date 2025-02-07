8f6d1af03b5db921964883dfba74ab15a69f335c
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-02-07 22:32:34 +0800

    Update the scrollbar style of the main layout to match the theme

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -18,12 +18,10 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
     setMounted(true);
   }, []);
 
+  if (!mounted) return null;
+
   const currentTheme = theme === "system" ? systemTheme : theme;
-  const spinnerColor = mounted
-    ? currentTheme === "dark"
-      ? "#6366f1"
-      : "#374151"
-    : "none";
+  const spinnerColor = currentTheme === "dark" ? "#6366f1" : "#374151";
 
   if (isLoading) {
     return (
@@ -37,13 +35,32 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
     return redirect("/");
   }
 
+  const isDark = currentTheme === "dark";
+  const trackColor = isDark ? "hsl(242, 47%,34%)" : "hsl(47, 24%, 70%)";
+  const thumbColor = isDark ? "hsl(245, 58%, 51%)" : "hsl(47, 71%, 88%)";
+
   return (
     <div className="flex h-full bg-gradient-to-br from-cornsilk-200 via-cornsilk-400 to-cornsilk-600 dark:from-indigo-950 dark:via-violet-950 dark:to-blue-950">
       <NavigationBar />
-      <main className="h-full flex-1 overflow-y-auto">
+      <main className="scrollbar-custom h-full flex-1 overflow-y-auto">
         <SearchCommand />
         {children}
       </main>
+
+      <style jsx global>{`
+        .scrollbar-custom::-webkit-scrollbar-track {
+          background: ${trackColor};
+        }
+
+        .scrollbar-custom::-webkit-scrollbar-thumb {
+          background-color: ${thumbColor};
+        }
+
+        .scrollbar-custom {
+          scrollbar-width: thin;
+          scrollbar-color: ${thumbColor} ${trackColor};
+        }
+      `}</style>
     </div>
   );
 };

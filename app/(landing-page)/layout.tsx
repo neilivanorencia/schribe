c682431cea9ae4a6d7bed54930ce480c83f6339e
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-02-12 13:32:12 +0800

    Integrate discover section into landing page

--- app/(landing-page)/layout.tsx
+++ app/(landing-page)/layout.tsx
@@ -1,25 +1,33 @@
-import { Header } from '@/app/(landing-page)/_components/header';
-import { Hero } from '@/app/(landing-page)/_components/hero';
-import { NavigationBar } from '@/app/(landing-page)/_components/navigation-bar';
+"use client";
+
+import { Discover } from "@/app/(landing-page)/_components/discover";
+import { Header } from "@/app/(landing-page)/_components/header";
+import { Hero } from "@/app/(landing-page)/_components/hero";
+import { NavigationBar } from "@/app/(landing-page)/_components/navigation-bar";
 
 const LandingPageLayout = () => {
-  const currentYear = new Date().getFullYear();
-
   return (
     <div className="min-h-screen bg-gradient-to-br from-cornsilk-200 via-cornsilk-400 to-cornsilk-600 dark:from-indigo-950 dark:via-violet-950 dark:to-blue-950">
       <div>
         <NavigationBar />
         <div className="pt-20">
-          <div className="mx-auto max-w-7xl px-4 py-10 sm:px-6 lg:px-8">
-            <div className="flex flex-col items-center justify-between gap-8 lg:flex-row lg:items-center">
-              <Header />
-              <Hero />
+          <section>
+            <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
+              <div className="flex flex-col items-center justify-between gap-8 lg:flex-row lg:items-center">
+                <Header />
+                <Hero />
+              </div>
             </div>
-            <footer className="mt-8 text-center text-sm text-gray-800 dark:text-gray-100 md:text-base">
-              Copyright &copy; {currentYear} Neil Ivan Orencia. All rights
-              reserved.
-            </footer>
-          </div>
+          </section>
+
+          <section
+            id="scroll-section"
+            className="bg-gradient-to-br from-cornsilk-50 via-cornsilk-200 to-cornsilk-400 dark:from-[#24205A] dark:via-[#38137C] dark:to-[#1F3270]"
+          >
+            <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
+              <Discover />
+            </div>
+          </section>
         </div>
       </div>
     </div>
